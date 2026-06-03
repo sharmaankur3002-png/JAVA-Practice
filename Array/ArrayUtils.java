08b10654bf785ac3a2b2aff7d886775e83d4e027
Ankur Sharma <sharmaankur3002@gmail.com>
2026-06-03 18:08:46 +0530

    Created ArrayUtils for reusable array code

---
+++ Array/ArrayUtils.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class ArrayUtils {
+
+    public static int[] createArray() {
+
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Enter Size Of Array : ");
+
+        int size = input.nextInt();
+
+        while (size <= 0) {
+
+            System.out.println("INPUT IS NOT VALID !");
+            System.out.print("Enter Size Of Array : ");
+
+            size = input.nextInt();
+
+        }
+
+        int[] newArr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter number in Array " + (i + 1) + ": ");
+            int num = input.nextInt();
+            newArr[i] = num;
+        }
+        return newArr;
+    }
+}
