19cdb522cbb06725ac7cc5df87f0f2b2a664cc30
Ankur Sharma <sharmaankur3002@gmail.com>
2026-06-05 13:11:19 +0530

    Add Create 2D Array Method .

--- Array/ArrayUtils.java
+++ Array/ArrayUtils.java
@@ -1,7 +1,11 @@
 
 import java.util.Scanner;
 
+
 public class ArrayUtils {
+
+
+// CREATE 1D ARRRAY CODE -
 
     public static int[] createArray() {
 
@@ -28,12 +32,38 @@ public class ArrayUtils {
         }
         return newArr;
     }
-  public static void printArray(int[] array) {
+
+// PRINT 1D ARRAY CODE-
+
+    public static void printArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
         System.out.println();
     }
+
+// CREATE 2D ARRAY CODE -
+
+    public static int[][] create2dArray() {
+
+        System.out.println("Enter size of 2D Array = ");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Rows Number :");
+        int rows = input.nextInt();
+        System.out.print("Enter Columns Number :");
+        int columns = input.nextInt();
+
+        int[][] numarray = new int[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print("Enter Element Row" + (i + 1) + ",colum" + (j + 1) + ":");
+                numarray[i][j] = input.nextInt();
+            }
+
+        }
+        return numarray;
     }
 
-
+}
