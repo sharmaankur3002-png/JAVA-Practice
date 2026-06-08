93fcd1554f7f949ed320441f9a49e3c8cefe55a8
Ankur Sharma <sharmaankur3002@gmail.com>
2026-06-08 18:57:29 +0530

    Add leap year checker using conditional statements

--- If-Else/LeapYear.java
+++ If-Else/LeapYear.java
@@ -6,16 +6,16 @@ public class LeapYear {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("--- CHECK YEAR IS LEAP ---");
+        System.out.println("--- CHECK YEAR IS LEAP ---");
         System.out.print("Enter year for check: ");
         int year = input.nextInt();
 
         if (year % 400 == 0) {
-            System.out.println("year is leap");
+            System.out.println("year is leap.");
         } else if (year % 100 == 0) {
-            System.out.println("year is not leap year");
+            System.out.println("year is not leap year.");
         } else if (year % 4 == 0) {
-            System.out.println("year is leap");
+            System.out.println("year is leap.");
         } else {
             System.out.println("year is not leap");
         }
