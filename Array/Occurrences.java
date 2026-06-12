54a751cd8f11f6a38179887b557c08587217744e
Ankur Sharma <sharmaankur3002@gmail.com>
2026-06-12 16:18:35 +0530

    using for-each to the occurrences of a specific element in an array

---
+++ Array/Occurrences.java
@@ -0,0 +1,28 @@
+
+// Q-- Create a program using for-each to the occurrences of a specific element in an array.
+
+import java.util.Scanner;
+
+public class Occurrences {
+
+    public static void main(String[] args) {
+        System.out.println("-- Occurrences of a specific element in an array --- ");
+
+        int[] array = ArrayUtils.createArray();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter element = ");
+        int num = input.nextInt();
+
+        int occ = 0;
+        for (int number : array) {
+
+            if (num == number) {
+                occ++;
+            }
+        }
+        System.out.println(num + " occurrences in araay = " + occ);
+
+    }
+}
